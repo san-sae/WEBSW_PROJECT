3f665d191073cb7a6ff09bd288810b4ab4849f58
choiyoo <choi.yoo.lim.12@gmail.com>
2024-06-01 22:05:30 +0900

    booklist 스타일링

--- react_project/src/pages/BookList.js
+++ react_project/src/pages/BookList.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './BookList.css';
 
+import Nav from '../component/Nav';
+import Header from '../component/Header';
+
+
 const mockBook = [
     {
         isbn: 9788932473901,
@@ -94,24 +98,32 @@ const mockBook = [
   
 function BookList() {
     return (
-      <div className="book-list">
-        {mockBook.map((book, index) => (
-          <div key={index} className="book-item">
-            <div className="book-image">
-              <img src={`${book.img}`} alt={book.name} />
-            </div>
-            <div className="book-info">
-              <h3>{book.name}</h3>
-              <p>저자: {book.author}</p>
-              <p>출판사: {book.publisher}</p>
-              <p>출간일: {book.date}</p>
-              <p>장르: {book.genre}</p>
-            </div>
-            <div className="review-button">
-              <button>리뷰 쓰기</button>
-            </div>
+      <div id="book-list">
+        <Header></Header>
+        <Nav></Nav>
+        <div className="list">
+          <h2 className="list-header">책 목록</h2>
+          <div className='list-content'>
+            <p className='text'><b>현재 리뷰가 가능한 책 목록입니다.</b></p>
+            {mockBook.map((book, index) => (
+              <div key={index} className="book-item">
+                <div className="book-image">
+                  <img src={`${book.img}`} alt={book.name} />
+                </div>
+                <div className="book-info">
+                  <h3>{book.name}</h3>
+                  <p>저자: {book.author}</p>
+                  <p>출판사: {book.publisher}</p>
+                  <p>출간일: {book.date}</p>
+                  <p>장르: {book.genre}</p>
+                </div>
+                <div className="review-button">
+                  <button className='btn'>리뷰 쓰기</button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     );
 }
