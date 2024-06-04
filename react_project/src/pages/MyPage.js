7319d381fe4501e7c2ea2fc8046ce3d3de4af4ef
MinKW1 <drone0101@naver.com>
2024-06-04 13:22:40 +0900

    added adding username to localStorage, need to edit mypage more

--- react_project/src/pages/MyPage.js
+++ react_project/src/pages/MyPage.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import Nav from './../component/Nav';
+import { useNavigate } from "react-router-dom";
+import Nav from './../component/Nav'
+import './MyPage.css';
 
 const MyPage = () => {
-  return (
-    <div>
-      <Nav />
-      <h2>마이페이지</h2>
-    </div>
-  );
+    const id = localStorage.getItem('id');
+    const password = localStorage.getItem('password');
+    const username = localStorage.getItem('username');
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate("/signin");
+    };
+
+    return (
+        <div className="display-container">
+            <Nav />
+            <br></br>
+            <div id="main_content">
+              <h2>사용자 정보 (임시)</h2>
+              <div className="data-group">
+                  <p><strong>사용자 이름</strong> {username}</p>
+                  <p><strong>ID:</strong> {id}</p>
+                  <p><strong>비밀번호:</strong> {password}</p>
+              </div>
+              <button className="backButton" onClick={goBack}>Go Back</button>
+            </div>
+        </div>
+    );
 };
 
 export default MyPage;
