68f29bf4c2a9f72d4832338a933f89fee4681675
MinKW1 <drone0101@naver.com>
2024-06-04 11:28:50 +0900

    plus info

--- react_project/src/pages/Question.js
+++ react_project/src/pages/Question.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import './Question.css';
 import Nav from './../component/Nav';
 
-
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     nickname: '',
@@ -37,26 +36,26 @@ const ContactForm = () => {
 
   return (
     <div className="container">
-     <Nav />
-
+      <Nav />
       <h1>Contact Us</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="nickname">닉네임 : </label>
+          <label htmlFor="nickname">닉네임  </label>
           <input type="text" id="nickname" name="nickname" value={formData.nickname} onChange={handleChange} required />
         </div>
         <div>
-          <label htmlFor="mail">이메일 : </label>
+          <label htmlFor="mail">이메일  </label>
           <input type="email" id="mail" name="mail" value={formData.mail} onChange={handleChange} required />
         </div>
         <div>
-          <label htmlFor="subject">제목 : </label>
+          <label htmlFor="subject">제목  </label>
           <input type="text" id="subject" name="subject" value={formData.subject} onChange={handleChange} required />
         </div>
         <div>
-          <label htmlFor="question">문의 내용 : </label>
+          <label htmlFor="question">문의 내용  </label>
           <textarea id="question" name="question" value={formData.question} onChange={handleChange} required></textarea>
         </div>
+        <br></br>
         <button type="submit" id="send_button">Send</button>
       </form>
     </div>
